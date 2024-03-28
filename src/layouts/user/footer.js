806d0fc0bf8e8c4dd542d7a3fc1c00f136edfc24
tiger <quiettiger020222@gmail.com>
2024-03-28 23:10:24 +0800

    header & footer

--- src/layouts/user/footer.js
+++ src/layouts/user/footer.js
@@ -1,13 +1,58 @@
 import React from "react";
+import logo from "../../public/assets/img/footer.png"
+import { FaTwitter, FaVideo, FaFacebook, FaInstagram } from 'react-icons/fa';
 
-const Footer = ({}) => {
+
+const Footer = ({ }) => {
 
     return (
-        <div className="w-full p-10">
-            Footer
-        
-        </div>
-    )
+        <footer className="xl:w-[1200px] lg:w-[992px] h-full md:w-9/12 w-11/12 mx-auto text-[#ccced8]">
+            <div className="grid grid-cols-12">
+                <img src={logo} className="lg:col-span-2 col-span-12 w-[72px] mb-5" />
+                <div className="lg:col-span-3 col-span-6">
+                    <h1 className="text-2xl pb-5 mb-2">Learn More</h1>
+                    <ul>
+                        <li className="pb-2 mb-2"><a href="#">About Volls</a></li>
+                        <li className="pb-2 mb-2"><a href="#">Policy</a></li>
+                        <li className="pb-2 mb-2"><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div className="lg:col-span-2 col-span-6">
+                    <h1 className="text-2xl pb-5 mb-2">Quick Links</h1>
+                    <ul>
+                        <li className="pb-2 mb-2"><a href="#">Services</a></li>
+                        <li className="pb-2 mb-2"><a href="#">Products</a></li>
+                        <li className="pb-2 mb-2"><a href="#">Cases</a></li>
+                    </ul>
+                </div>
+                <div className="lg:hidden block col-span-12 my-8">
+                    <div className="lg:mt-[90px] mt-[75px] border-b-1 border border-solid border-white border-1">
+                        <div></div>
+                    </div>
+                </div>
+                <div className="lg:col-span-3 col-span-6">
+                    <h1 className="text-2xl pb-5 mb-2">Contact Us</h1>
+                    <a className="pb-2 mb-2" href="#">infovolls.eu</a>
+                </div>
+                <div className="lg:col-span-2 col-span-12">
+                    <h1 className="text-2xl pb-5 mb-2">Social</h1>
+                    <div className="flex gap-3 ">
+                        <FaFacebook size={20} className="text-white hover:text-[#ffdd59] mat-icon-no-color"/>
+                        <FaVideo size={20} className="text-white hover:text-[#ffdd59] mat-icon-no-color"/>
+                        <FaTwitter size={20} className="text-white hover:text-[#ffdd59] mat-icon-no-color"/>
+                        <FaInstagram size={20} className="text-white hover:text-[#ffdd59] mat-icon-no-color" />
+                    </div>
+                </div>
+
+            </div>
+            <div className="lg:mt-[90px] mt-[75px] border-b-1 border border-solid border-white border-1">
+                <div></div>
+            </div>
+            <div className="flex justify-center items-center my-7">
+                <p className="text-lg">© 2022 — Voll's Tech</p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/layouts/user/header.js
+++ src/layouts/user/header.js
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../../public/assets/img/footer.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Header = ({ }) => {
 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+        // Add your sidebar toggle logic here
+    };
     return (
-        <div className="w-full p-10">
-            Header
-        
+        <div className="xl:w-[1200px] w-11/12 mx-auto h-[94px] text-white bg-transparent flex items-end">
+            <div className="flex justify-between w-full">
+                <img className="w-[74px] ml-10 mr-20" src={logo} />
+                <ul className={`justify-between items-center lg:gap-14 md:gap-8 gap-4 md:flex hidden`}>
+                    <li className="text-sm leading-[30px]"><a href="#" className="hover:underline hover:text-[#ffdd59]">Services</a></li>
+                    <li className="text-sm leading-[30px]"><a href="#" className="hover:underline hover:text-[#ffdd59]">Products</a></li>
+                    <li className="text-sm leading-[30px]"><a href="#" className="hover:underline hover:text-[#ffdd59]">Cases</a></li>
+                    <li className="text-sm leading-[30px]"><a href="#" className="hover:underline hover:text-[#ffdd59]">Partners</a></li>
+                    <li className="text-sm leading-[30px]"><a href="#" className="hover:underline hover:text-[#ffdd59]">Contacts</a></li>
+                </ul>
+
+                <div className="items-center gap-5 md:flex hidden">
+                    <p href="#" className="text-base leading-[22px]">Contacts</p>
+                    <button className="flex justify-center items-center transition ease-in-out duration-300 border-2 border-primary w-[30px] h-[30px] rounded-full hover:bg-white bg-transparent text-primary hover:text-black"><FontAwesomeIcon icon={faArrowRight} /></button>
+                </div>
+                <div className="md:hidden block">
+                    <div className="">
+                        {isOpen ? (
+                            <AiOutlineClose size={30} color="#fff" onClick={toggleNavbar} />
+                        ) : (
+                            <AiOutlineMenu size={30} color="#fff" onClick={toggleNavbar} />
+                        )}
+                    </div>
+                </div>
+
+            </div>
+            <nav className={`md:hidden lg:hidden fixed top-0 left-0 h-full bg-slate-900 text-white w-64 z-10 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition duration-300 ease-in-out`}>
+                <div className="p-4 ">
+                <img className="w-[74px] ml-10 mr-20 pb-5 pt-2" src={logo} />
+                    <ul className={`justify-between text-white `}>
+                        <li className="text-sm leading-[30px] pb-4"><a href="#" className="hover:underline hover:text-[#ffdd59]">Services</a></li>
+                        <li className="text-sm leading-[30px] pb-4"><a href="#" className="hover:underline hover:text-[#ffdd59]">Products</a></li>
+                        <li className="text-sm leading-[30px] pb-4"><a href="#" className="hover:underline hover:text-[#ffdd59]">Cases</a></li>
+                        <li className="text-sm leading-[30px] pb-4  "><a href="#" className="hover:underline hover:text-[#ffdd59]">Partners</a></li>
+                        <li className="text-sm leading-[30px] pb-4"><a href="#" className="hover:underline hover:text-[#ffdd59]">Contacts</a></li>
+                    </ul>
+                    <div className="items-center gap-5 flex md:hidden">
+                        <p href="#" className="text-base leading-[22px]">Contacts</p>
+                        <button className="flex justify-center items-center transition ease-in-out duration-300 border-2 border-primary w-[30px] h-[30px] rounded-full hover:bg-primary bg-transparent text-primary hover:text-black"><FontAwesomeIcon icon={faArrowRight} /></button>
+                    </div>
+                </div>
+            </nav>
         </div>
-    )
+    );
 }
 
 export default Header;
