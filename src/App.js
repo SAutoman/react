98aa397b824eb2bf49dc7140c819442c1270b289
richard <richardwang981219@gmail.com>
2024-03-29 01:14:49 -0700

    push

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/admin' element={<DashBoard />} />
           <Route path='/'  element= {<Home />}/>
+          
         </Routes>
       </Router>
     </div>
